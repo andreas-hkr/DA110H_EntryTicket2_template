863edc00df153d821254e54fafcfe13a58816384
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-07-04 10:00:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def print_menu():
+    pass        # Replace pass with your code
+
+
+def new_test_subject():
+    pass        # Replace pass with your code
+
+
+def write_data(test_subjects, filename):
+    pass        # Replace pass with your code
+
+
+def read_data(filename):
+    pass        # Replace pass with your code
+
+
+def print_test_subjects(test_subjects):
+    pass        # Replace pass with your code
+
+
+def main():
+    pass        # Replace pass with your code
+
+
+if __name__ == '__main__':
+    main()
